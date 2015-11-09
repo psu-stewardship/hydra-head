95ff99ef4903e6b994023536f5949ed0fbde94c0
Carolyn Cole <cam156@psu.edu>
2015-11-09 12:39:03 -0500

    Changing Blacklight::Solr::SearchBuilder to Blacklight::SearchBuilder

--- hydra-core/app/search_builders/hydra/search_builder.rb
+++ hydra-core/app/search_builders/hydra/search_builder.rb
@@ -1,5 +1,5 @@
 module Hydra
-  class SearchBuilder < Blacklight::Solr::SearchBuilder
+  class SearchBuilder < Blacklight::SearchBuilder
     self.default_processor_chain += [:add_access_controls_to_solr_params]
     include Hydra::AccessControlsEnforcement
   end
